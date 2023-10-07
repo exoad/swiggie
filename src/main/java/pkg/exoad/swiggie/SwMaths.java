d5841ab50c414651aa270a31c1bd9558d30d4985
Jack Meng <jackmeng0814@gmail.com>
2023-10-07 17:25:02 -0400

    `add` amend a new math class for vector

---
+++ src/main/java/pkg/exoad/swiggie/SwMaths.java
@@ -0,0 +1,17 @@
+package pkg.exoad.swiggie;
+
+public final class SwMaths
+{
+	private SwMaths()
+	{}
+
+	/**
+	 * Factor for converting from Radians to Degrees
+	 */
+	public static final float RAD_TO_DEG = (float) (180F / Math.PI);
+
+	/**
+	 * Factor for converting from Degrees to Radians
+	 */
+	public static final float DEG_TO_RAD = (float) (Math.PI / 180F);
+}
---
+++ src/main/java/pkg/exoad/swiggie/stx/SwVector2.java
@@ -0,0 +1,107 @@
+package pkg.exoad.swiggie.stx;
+
+import lombok.Getter;
+import lombok.Setter;
+import pkg.exoad.swiggie.SwMaths;
+
+@Getter @Setter public class SwVector2
+{
+	public static SwVector2 acquire(float x, float y)
+	{
+		return new SwVector2(x, y);
+	}
+
+	private float x;
+	private float y;
+
+	private SwVector2(float x, float y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+
+	public float length()
+	{
+		return (float) Math.sqrt(x * x + y * y);
+	}
+
+	public float distance(float x, float y)
+	{
+		return (float) Math.sqrt((x - this.x) * (x - this.x) + (y - this.y) * (y - this.y));
+	}
+
+	public float distance(SwVector2 other)
+	{
+		return (float) Math.sqrt((other.x - this.x) * (other.x - this.x) + (other.y - this.y) * (other.y - this.y));
+	}
+
+	public float cross(float x, float y)
+	{
+		return this.x * y - this.y * x;
+	}
+
+	public float cross(SwVector2 other)
+	{
+		return this.x * other.y - this.y * other.x;
+	}
+
+	public SwVector2 subtract(float x, float y)
+	{
+		this.x -= x;
+		this.y -= y;
+		return this;
+	}
+
+	public SwVector2 subtract(SwVector2 other)
+	{
+		this.x -= other.x;
+		this.y -= other.y;
+		return this;
+	}
+
+	public SwVector2 add(float x, float y)
+	{
+		this.x += x;
+		this.y += y;
+		return this;
+	}
+
+	public SwVector2 add(SwVector2 other)
+	{
+		this.x += other.x;
+		this.y += other.y;
+		return this;
+	}
+
+	public float dot(float x, float y)
+	{
+		return this.x * x + this.y * y;
+	}
+
+	public float dot(SwVector2 other)
+	{
+		return this.x * other.x + this.y * other.y;
+	}
+
+	public float angleDegToX()
+	{
+		float e = (float) Math.atan2(y, x) * SwMaths.RAD_TO_DEG;
+		return e < 0 ? e + 360 : e;
+	}
+
+	public float angleDegTo(SwVector2 other)
+	{
+		return (float) Math.atan2(cross(other), dot(other)) * SwMaths.RAD_TO_DEG;
+	}
+
+	public float angleRadToX()
+	{
+		return (float) Math.atan2(y, x);
+	}
+
+	public float angleRadTo(SwVector2 other)
+	{
+		return (float) Math.atan2(cross(other), dot(other));
+	}
+
+}
